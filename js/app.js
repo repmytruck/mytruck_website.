d5e86be75aef4ca23ac50f63e75b41120bcc7573
Dyhogenys <dyhogenys.dg@gmail.com>
2018-09-04 14:14:05 -0300

    primeiro commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+ //conectando-se ao firebase
+ const config = {
+    apiKey: "####",
+    authDomain: "####",
+    databaseURL: "####",
+    projectId: "####",
+    storageBucket: "####",
+    messagingSenderId: "####"
+  };
+  //iniciando o firebase
+  firebase.initializeApp(config);
